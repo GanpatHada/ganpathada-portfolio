b273fa81f348ac29645059868057ecf150563d31
GanpatHada <hadaganpat42@gmail.com>
2022-09-25 00:20:09 +0530

    styled footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react'
 const Footer = () => {
   return (
     <footer>
-        <h3>Follow me and Connect on</h3>
+        <h3>Follow me on</h3>
         <div>
         <a href="https://www.linkedin.com/in/ganpat-hada-33abb4227" target="_blank"><i className="fa-brands fa-linkedin"></i></a>
         <a href="https://github.com/GanpatHada" target="_blank"><i className="fa-brands fa-github"></i></a>
@@ -12,7 +12,7 @@ const Footer = () => {
         <a href="/" target="_blank"><i class="fa-brands fa-instagram"></i></a>
         </div>
         <div>
-            <p>© 2022, Built with ❤ by Ganpat Hada</p>
+            <p>© 2022 all rights reserved , Built with ❤ by Ganpat Hada</p>
         </div>
     </footer>
   )
