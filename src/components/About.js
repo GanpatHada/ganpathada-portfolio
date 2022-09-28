6ddc9df2993c2f6bfc2c7306e4b76026f01f8113
GanpatHada <hadaganpat42@gmail.com>
2022-09-28 17:43:29 +0530

    fix footer

--- src/components/About.js
+++ src/components/About.js
@@ -5,13 +5,13 @@ const About = () => {
     <div id="about" className='mainblocks'>
       <h1><span>M</span>e and <span>M</span>yself</h1>
       <div>
-        Hi, I am Ganpat Hada a Full stack  Developer. I have been working on the MERN stack for about 4 months in this span I have developed multiple projects which helped me to enhance my skills. I am open to adapting to whichever framework is required. <br /><br />
+        <p>Hi, I am Ganpat Hada a Full stack  Developer. I have been working on the MERN stack for about 4 months in this span I have developed multiple projects which helped me to enhance my skills. I am open to adapting to whichever framework is required. <br /><br />
 
-        Before all this, I have learned c,c++ and java and did some competitive programming and dsa ,I also have projects in java swing using mysql and in c++ using file handling <br />
+          Before all this, I have learned c,c++ and java and did some competitive programming and dsa ,I also have projects in java swing using mysql and in c++ using file handling <br />
 
-        I believe Project Based Learning is the best approach to learn anything. Hence, I've got projects in all the different technologies that I've learned over time. <br /><br />
+          I believe Project Based Learning is the best approach to learn anything. Hence, I've got projects in all the different technologies that I've learned over time. <br /><br />
 
-        By the way, when I'm not coding, you'll find me painting,sketching, or listening songs :
+          By the way, when I'm not coding, you'll find me painting,sketching, or listening songs :</p>
       </div>
     </div>
   )
