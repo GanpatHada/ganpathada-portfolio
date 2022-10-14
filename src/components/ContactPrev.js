686916a00355530f68b4b8f6642931ad2d75bbf1
GanpatHada <hadaganpat42@gmail.com>
2022-10-15 01:28:51 +0530

    fixed style section

--- src/components/ContactPrev.js
+++ src/components/ContactPrev.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import contact from './web logos/contact.svg'
 import gmail from './web logos/gmail.png'
-const ContactPrev = ({scroll}) => {
+const ContactPrev = ({ scroll }) => {
     return (
         <div className='prevsblock' id='contactprev' ref={scroll}>
             <div className='imagesection'>
@@ -12,10 +12,10 @@ const ContactPrev = ({scroll}) => {
                 <p>I'd love to hear about what you're working on and how I could help. I'm also open to discussing ideas and doing a project together. Drop me a mail or DM me on LinkedIn and let's have a talk.
                     Feel free to drop a message
                 </p>
-                <div className='btngroup'>
-                    <a href="https://www.linkedin.com/in/ganpat-hada-33abb4227" target='_blank'><button className='primarybtn'>Linked <i className="fa-brands fa-linkedin"></i></button>&nbsp;</a>
 
-                </div>
+                <a href="https://www.linkedin.com/in/ganpat-hada-33abb4227" target='_blank'><button className='primarybtn'>Linked <i className="fa-brands fa-linkedin"></i></button>&nbsp;</a>
+
+
             </div>
         </div>
     )
