a0cc9d17f11c5393bc3511f6f2b45b5757bd6d44
GanpatHada <hadaganpat42@gmail.com>
2022-12-08 14:58:58 +0530

    corrected welcome message

--- src/components/Main.js
+++ src/components/Main.js
@@ -7,7 +7,7 @@ const Main = () => {
             <main className='prevsblock' id='mainblock'>
                 <div className="introsection">
                     <h2>Hi there !</h2>
-                    <h2>I'am Ganpat Hada a <span>web developer</span></h2>
+                    <h2>I'am Ganpat Hada , a <span>web developer</span></h2>
                     <p>Final year BCA Hons. student at DAVV Indore,currently exploring Typescript and figma</p>
                     <Link to="/about"><button className='primarybtn'>Know more</button></Link>
                 </div>
